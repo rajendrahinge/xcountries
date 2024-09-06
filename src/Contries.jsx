f7bc2bc055b6e48d205e37ae57420363a5b11546
rajendrahinge <rajendrahinge24@gmail.com>
2024-09-07 03:06:28 +0530

    push to prod

---
+++ src/Contries.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import ContriesCard from './ContriesCard';
+
+function Contries() {
+    const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
+    const [flags, setFlags] = useState([]);
+
+    // console.log({flags});
+
+    useEffect(() => {
+        fetch(API_ENDPOINT)
+        .then((res)=>res.json())
+        .then((data) => setFlags(data))
+        .catch((error) => console.log("error feching data: ",error));
+    },[]);
+
+
+    
+    return <div style={{
+        display:"flex",
+        flexWrap:"wrap"
+    }}
+    >
+        {flags.map((flag) => (
+                <ContriesCard   
+                    name={flag.name} 
+                    flagImg={flag.flag} 
+                    flagAltText={flag.abbr} 
+                    />
+            ))}
+        </div>
+}
+
+export default Contries;
---
+++ src/ContriesCard.jsx
@@ -0,0 +1,26 @@
+
+const ContriesCard = ({name,flagImg,flagAltText}) => {
+    return <div
+            style={{
+                display:"flex",
+                flexDirection:"column",
+                justifyContent:"center",
+                alignItems:"center",
+                padding:"10px",
+                margin:"10px",
+                border: "1px solid gray",
+                borderRadius: "4px",
+                width: "200px",
+                height: "200px"
+            }}
+        >
+        <img src={flagImg} style={{
+            height: "100px", width: "100px"
+        }} alt={flagAltText}/>
+        <h2>{name}</h2>
+    </div>
+}
+
+
+
+export default ContriesCard;
